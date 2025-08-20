ca5e974399023800bcc418b19993a0e9e6a8344b
mendyhac <mendykanal@gmail.com>
2025-08-20 19:29:59 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 import sqlite3
@@ -117,3 +116,31 @@ async def premium(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 async def check_payment(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+user_id = update.message.from_user.id
+    
+    cursor.execute(
+        "UPDATE users SET premium = 1 WHERE user_id = ?",
+        (user_id,)
+    )
+    conn.commit()
+    
+    await update.message.reply_text(
+        "✅ Premium-доступ активовано!\nТепер у тебе безлімітні повідомлення!",
+        reply_markup=main_menu()
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("premium", premium))
+    app.add_handler(CommandHandler("check_payment", check_payment))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_buttons))
+    
+    print("🟢 Бот запущено!")
+    app.run_polling()
+
+if name == "main":
+    main()
+
